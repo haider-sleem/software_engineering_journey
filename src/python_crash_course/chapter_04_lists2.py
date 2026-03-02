633b39c333d0b61c9906629b07c9a216e9c9d7b9
Haider Sleem <haidersleem@gmail.com>
2026-03-03 00:09:23 +0200

    Finish Chapter 4 exercises: Slices and Tuples

--- src/python_crash_course/chapter_04_lists2.py
+++ src/python_crash_course/chapter_04_lists2.py
@@ -26,11 +26,11 @@
 # for i in range (1,1000001):
 #      print(i)
 
-# 4-5
-numbers =  range (1,1000001)
-print(min(numbers))
-print(max(numbers))
-print(sum(numbers))
+# 4-5    # برنامج التيست 
+# numbers = range(1, 1000001)
+# print(min(numbers))
+# print(max(numbers))
+# print(sum(numbers))
 
 # 4-6
 # for i in range (1,20,2) :
@@ -50,4 +50,53 @@ print(sum(numbers))
 # cubed_numbers = [i ** 3 for i in range (1,10)]
 # print(cubed_numbers)
 
-# stoped page 99
+
+# TRY IT YOURSELF page 103
+
+# 4-10
+# favorite_pizza = ["Margherita","Pepperoni","BBQ Chicken","Cheese","Vegetarian"]
+# print(f"The first three items in the list are: {favorite_pizza[:3]}")
+
+# print(f"\nThree items from the middle of the list are: {favorite_pizza[1:4]}")
+
+# print(f"\nThe last three items in the list are: {favorite_pizza[-3:]}")
+
+# 4-11
+# my_pizza = ["Margherita","Pepperoni","BBQ Chicken"]
+
+# friend_pizza = my_pizza[:]
+
+# my_pizza.append("Vegetarian")
+# friend_pizza.append("Cheese")
+
+# print("My favorite pizzas are:")
+# for pizza in my_pizza :
+#     print(pizza)
+
+# print("\nMy friend’s favorite pizzas are:")
+# for pizza in friend_pizza :
+#     print(pizza)
+
+
+# 4-12
+# favorite_pizza = ["Margherita","Pepperoni","BBQ Chicken","Cheese","Vegetarian"]
+
+# for pizza in favorite_pizza :
+#     print(pizza)
+
+
+# TRY IT YOURSELF    page 106
+
+# 4-13
+# foods = ("rice", "chicken", "salad", "bread", "soup")
+# for food in foods :
+#     print(food)
+
+# foods[1] = "meat"    #   التأكد من أن التوبل لا تقبل التعديل
+# print(foods)
+
+# foods = ("rice", "chicken", "salad", "juice", "meat")
+# for food in foods :
+#     print (food)
+
+
