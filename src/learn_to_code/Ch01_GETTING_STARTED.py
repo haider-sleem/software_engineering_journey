60fda722bb852e6430a1731393f3aa988a5e4055
Haider Sleem <haidersleem@gmail.com>
2026-03-07 01:03:48 +0200

    Add: Core Drill problem solution to Chapter 1 exercises

--- src/learn_to_code/Ch01_GETTING_STARTED.py
+++ src/learn_to_code/Ch01_GETTING_STARTED.py
@@ -15,3 +15,17 @@
 
 
 # problem dmopc14c5p1
+
+# PI = 3.141592653589793
+
+# radius = int(input())
+# height = int(input())
+
+# volume = (PI * radius ** 2 * height) / 3
+
+# print(volume)
+
+
+#############################
+
+# Chapter Exercises page 57
