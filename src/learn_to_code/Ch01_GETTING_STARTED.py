058ee88d0eae0375bcfe11c8664eb961bdc19eea
Haider Sleem <haidersleem@gmail.com>
2026-03-06 02:19:28 +0200

    Complete problem 1: Solve Not a Wall of Text & update notes

--- src/learn_to_code/Ch01_GETTING_STARTED.py
+++ src/learn_to_code/Ch01_GETTING_STARTED.py
@@ -1,1 +1,17 @@
-# Chapter 1 Notes
+# (((((الفصل الأول)))))
+
+# dmopc15c7p2
+# line = input()
+# total_words = line.count(" ") + 1
+# print(total_words)
+
+
+# حل تاني في حالة لو أن المسافات غير منتظمة
+# line = input()
+# total_words = len(line.split())
+# print(total_words)
+
+####################################################
+
+
+# problem dmopc14c5p1
