e2793a217ee632ec7bcd43a647e056176a055870
Haider Sleem <haidersleem@gmail.com>
2026-03-12 22:57:55 +0200

    Work on Chapter 5: Conditional tests part

---
+++ src/python_crash_course/chapter_05_if.py
@@ -0,0 +1,62 @@
+# TRY IT YOURSELF page 115
+# 5-1 & 5-2
+
+# example : A
+# new_arrival = "omar"
+# print("Is new_arrival == 'omar'? I predict True.")
+# print(new_arrival == "omar")
+
+# print("\nIs new_arrival == 'ahmed'? I predict False.")
+# print(new_arrival == "ahmed")
+
+# example : B
+# age = 25
+# print("Is age > 18? I predict True.")
+# print(age > 18)
+
+# print("\nIs age < 10? I predict False.")
+# print(age < 10)
+
+# example : C
+# name = "Haider"
+# print("Is name.lower() == 'haider'? I predict True.")
+# print(name.lower() == "haider")
+
+# print("\nIs name == 'haider'? I predict False.")
+# print(name == "haider")
+
+# example : D
+# users_count = 100
+# print("Is users_count != 100? I predict False.")
+# print(users_count != 100)
+
+# print("\nIs users_count != 200? I predict True.")
+# print(users_count != 200)
+
+# example : E
+# age_0 = 22
+# age_1 = 18
+
+# print("Is age_0 >= 21 and age_1 >= 21? I predict False.")
+# print(age_0 >= 21 and age_1 >= 21)
+
+# print("\nIs age_0 >= 21 or age_1 >= 21? I predict True.")
+# print(age_0 >= 21 or age_1 >= 21)
+
+# print("\nIs age_0 > 20 and age_1 < 20? I predict True.")
+# print(age_0 > 20 and age_1 < 20)
+
+
+# example : F
+# banned_users = ['andrew', 'carolina', 'david']
+# user = 'marie'
+
+# print("Is user not in banned_users? I predict True.")
+# print(user not in banned_users)
+
+# user_to_check = 'david'
+# print("\nIs 'david' in banned_users? I predict True.")
+# print(user_to_check in banned_users)
+
+# print("\nIs 'haider' in banned_users? I predict False.")
+# print('haider' in banned_users)
