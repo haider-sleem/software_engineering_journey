e0aee438c89527c91ab5f7a42d6c8d3ece3a1135
Haider Sleem <haidersleem@gmail.com>
2026-03-29 06:39:27 +0200

    feat(Ch02): solve Winning Score with basic and modular approaches

---
+++ src/learn_to_code/Ch02_MAKING_DECISIONS.py
@@ -0,0 +1,22 @@
+# Problem #3: Winning Team (DMOJ problem ccc19j1.) page 58
+
+# طريقة حل المسألة في الكتاب أنظر ملف راجع ملف إم دي للطريقة الإحترافية و تحليلها
+
+# apple_three = int(input())
+# apple_two = int(input())
+# apple_one = int(input())
+# banana_three = int(input())
+# banana_two = int(input())
+# banana_one = int(input())
+# apple_total = apple_three * 3 + apple_two * 2 + apple_one
+# banana_total = banana_three * 3 + banana_two * 2 + banana_one
+# if apple_total < banana_total:
+#     print("B")
+# elif apple_total > banana_total:
+#     print("A")
+# else:
+#     print("T")
+
+
+# -----------------------------------------------
+
