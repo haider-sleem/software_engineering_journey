1f1f6c495021e7d4ac7e2fb62df31f3a15e943b5
Haider Sleem <haidersleem@gmail.com>
2026-08-31 11:13:49 +0300

    feat(GrokkingAlgo-Ch01-src): add chapter 01 introduction to algorithms script

---
+++ 01_roadmap/phase_02_dsa_and_python_engineering/grokking_algorithms/src/ch01_introduction_to_algorithms.py
@@ -0,0 +1,18 @@
+def binary_search(arr, item):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = arr[mid]  
+
+        if guess == item:
+            return mid    
+        if guess > item:
+            high = mid - 1  
+        else:
+            low = mid + 1   
+
+    return None
+
+print(binary_search([1, 2, 3, 4, 5], 3))
